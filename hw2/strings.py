61adfd40dbe72dd5e8d30504e0ab1753b390563e
Polina Vorobeva <vpd.polina@gmail.com>
2019-11-14 14:35:34 +0200

    with list comprehention

--- hw2/strings.py
+++ hw2/strings.py
@@ -2,11 +2,7 @@ from typing import List
 
 
 def filter_lines_of_length_five(lines: List[str]) -> List[str]:
-    list_of_fives = []
-    for i in range(len(lines)):
-        if len(lines[i]) == 5:
-            list_of_fives.append(lines[i])
-    return list_of_fives
+    return [line for line in lines if len(line) == 5]
 
 
 if __name__ == '__main__':
