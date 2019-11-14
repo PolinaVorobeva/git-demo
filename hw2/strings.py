a292ab596776d568cae82d3cd9d0feeef9666cc9
Polina Vorobeva <vpd.polina@gmail.com>
2019-11-14 14:19:16 +0200

    Implement function

--- hw2/strings.py
+++ hw2/strings.py
@@ -2,7 +2,11 @@ from typing import List
 
 
 def filter_lines_of_length_five(lines: List[str]) -> List[str]:
-    pass
+    list_of_fives = []
+    for i in range(len(lines)):
+        if len(lines[i]) == 5:
+            list_of_fives.append(lines[i])
+    return list_of_fives
 
 
 if __name__ == '__main__':
